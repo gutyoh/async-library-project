a21407d929b8eaebd8587ea4bda5e81e1a938596
Hermann Rösch <mr.guty@Hotmail.com>
2022-11-20 13:08:39 -0500

    Solution for Stage #2

--- alog.go
+++ alog.go
@@ -24,19 +24,32 @@ type Alog struct {
 
 // New creates a new Alog object that writes to the provided io.Writer.
 // If nil is provided the output will be directed to os.Stdout.
+// 2.1 - Create the message channel
+// 2.2 - Create the error channel
+// 2.8 - Protect against concurrent writes to log
 func New(w io.Writer) *Alog {
 	if w == nil {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest: w,
+		dest:    w,
+		m:       &sync.Mutex{},     // 2.8.1 - Initialize the `m` field of `Alog` with a new sync.Mutex
+		msgCh:   make(chan string), // 2.1.1 - Initialize the message channel
+		errorCh: make(chan error),  // 2.2.1 - Create the error channel
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
+// 2.7 - Update Start() method to call `write()` when message comes in
 func (al Alog) Start() {
-
+	// 2.7.1 - Create an infinite loop that receives messages from `al.msgCh`
+	for {
+		// 2.7.2 - When a message arrives, a goroutine should pass the message along to the `write()` method
+		// 2.7.2 - However, for Stage #2, just pass `nil` to replace the `wg` argument -- we won't use goroutines yet
+		msg := <-al.msgCh
+		go al.write(msg, nil) // 2.7.2 - Start a goroutine that passes the message to `write()`
+	}
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -46,22 +59,39 @@ func (al Alog) formatMessage(msg string) string {
 	return fmt.Sprintf("[%v] - %v", time.Now().Format("2006-01-02 15:04:05"), msg)
 }
 
+// 2.5 - Implement write() method
+// 2.6 - Add error handling to write() method
+// 2.8 - Protect against concurrent writes to log
+// 2.9 - Refactor error reporting
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	al.m.Lock()         // 2.8.2 - Lock the mutex before the `Write()` method call
+	defer al.m.Unlock() // 2.8.3 - Unlock the mutex after the `Write()` method call
+
+	// 2.5.1 - Format the `msg` argument using `formatMessage()`
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	if err != nil { // 2.6.1 - Capture the error `err` and if it is not `nil` send it to `al.errorCh`
+		// 2.9.1 - Use a goroutine to send the error to `al.errorCh`
+		go func(err error) {
+			al.errorCh <- err
+		}(err)
+	}
 }
 
 func (al Alog) shutdown() {
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
-func (al Alog) MessageChannel() chan string {
-	return nil
+// 2.3 - Implement the MessageChannel() accessor method
+func (al Alog) MessageChannel() chan<- string { // 2.3.2 Return a send-only channel
+	return al.msgCh // 2.3.1 - Return the message channel
 }
 
 // ErrorChannel returns a channel that will be populated when an error is raised during a write operation.
 // This channel should always be monitored in some way to prevent deadlock goroutines from being generated
 // when errors occur.
-func (al Alog) ErrorChannel() chan error {
-	return nil
+// 2.4 - Implement the ErrorChannel() accessor method
+func (al Alog) ErrorChannel() <-chan error { // 2.4.2 Return a receive-only channel
+	return al.errorCh // 2.4.1 - Return the error channel
 }
 
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
