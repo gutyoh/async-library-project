b87c2dde28a48dc044ce056c683e747f86be6fb9
Hermann Rösch <mr.guty@Hotmail.com>
2022-11-20 13:11:27 -0500

    Added solution for Stage #3

--- alog.go
+++ alog.go
@@ -24,31 +24,48 @@ type Alog struct {
 
 // New creates a new Alog object that writes to the provided io.Writer.
 // If nil is provided the output will be directed to os.Stdout.
-// 2.1 - Create the message channel
-// 2.2 - Create the error channel
-// 2.8 - Protect against concurrent writes to log
+// 3.1 - Create channels to shutdown logger
 func New(w io.Writer) *Alog {
 	if w == nil {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest:    w,
-		m:       &sync.Mutex{},     // 2.8.1 - Initialize the `m` field of `Alog` with a new sync.Mutex
-		msgCh:   make(chan string), // 2.1.1 - Initialize the message channel
-		errorCh: make(chan error),  // 2.2.1 - Create the error channel
+		dest:               w,
+		m:                  &sync.Mutex{},
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		shutdownCh:         make(chan struct{}), // 3.1.1 - Initialize the shutdown channel
+		shutdownCompleteCh: make(chan struct{}), // 3.1.2 - Initialize the shutdown complete channel
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
-// 2.7 - Update Start() method to call `write()` when message comes in
+// 3.3 - Update Start() to process messages from shutdownCh
+// 3.5 - Wait for all log entries to be written before shutting down
 func (al Alog) Start() {
-	// 2.7.1 - Create an infinite loop that receives messages from `al.msgCh`
+	wg := &sync.WaitGroup{} // 3.5.1 - Create a `sync.WaitGroup` to track the number of pending log entries
+
 	for {
-		// 2.7.2 - When a message arrives, a goroutine should pass the message along to the `write()` method
-		// 2.7.2 - However, for Stage #2, just pass `nil` to replace the `wg` argument -- we won't use goroutines yet
-		msg := <-al.msgCh
-		go al.write(msg, nil) // 2.7.2 - Start a goroutine that passes the message to `write()`
+		// 3.3.1 - Add a `select` statement to the loop so messages can be processed from
+		// the `al.msgCh` channel and the `al.shutdownCh` channel
+		select {
+		// 3.3.2 - If a message is received on the `al.msgCh` call `al.write()` to write the message to the log
+		case msg := <-al.msgCh:
+			wg.Add(1) // 3.5.2 - Increment the `WaitGroup` counter before calling `al.write()`
+
+			// 3.5.3 - Pass the `WaitGroup` to `al.write()` to call `wg.Done()` when the message has been written
+			go al.write(msg, wg)
+
+		// 3.3.3 - If a message is received on the `al.shutdownCh` channel, call `al.shutdown()`
+		// and break out of the infinite loop
+		case <-al.shutdownCh:
+			// 3.5.5 - Call the `Wait()` method on the `WaitGroup` when a message is received by `Start()`
+			// but before it calls `al.shutdown()`
+			wg.Wait()
+			al.shutdown()
+			return
+		}
 	}
 }
 
@@ -59,44 +76,51 @@ func (al Alog) formatMessage(msg string) string {
 	return fmt.Sprintf("[%v] - %v", time.Now().Format("2006-01-02 15:04:05"), msg)
 }
 
-// 2.5 - Implement write() method
-// 2.6 - Add error handling to write() method
-// 2.8 - Protect against concurrent writes to log
-// 2.9 - Refactor error reporting
+// 3.5 - Wait for all log entries to be written before shutting down
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
-	al.m.Lock()         // 2.8.2 - Lock the mutex before the `Write()` method call
-	defer al.m.Unlock() // 2.8.3 - Unlock the mutex after the `Write()` method call
+	// 3.5.4 - Defer the call on the `Done()` method on the `WaitGroup` after the message has been written
+	defer wg.Done()
 
-	// 2.5.1 - Format the `msg` argument using `formatMessage()`
+	al.m.Lock()
+	defer al.m.Unlock()
+
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
-	if err != nil { // 2.6.1 - Capture the error `err` and if it is not `nil` send it to `al.errorCh`
-		// 2.9.1 - Use a goroutine to send the error to `al.errorCh`
+	if err != nil {
 		go func(err error) {
 			al.errorCh <- err
 		}(err)
 	}
 }
 
+// 3.2 Implement shutdown() method
+// 3.4 Implement the Stop() method
 func (al Alog) shutdown() {
+	// 3.2.1 - Close the message channel to prevent the logger from receiving new messages:
+	close(al.msgCh)
+	// 3.4.3 - Send a finished message on the shutdown complete channel to indicate that the shutdown is complete:
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
-// 2.3 - Implement the MessageChannel() accessor method
-func (al Alog) MessageChannel() chan<- string { // 2.3.2 Return a send-only channel
-	return al.msgCh // 2.3.1 - Return the message channel
+func (al Alog) MessageChannel() chan<- string {
+	return al.msgCh
 }
 
 // ErrorChannel returns a channel that will be populated when an error is raised during a write operation.
 // This channel should always be monitored in some way to prevent deadlock goroutines from being generated
 // when errors occur.
-// 2.4 - Implement the ErrorChannel() accessor method
-func (al Alog) ErrorChannel() <-chan error { // 2.4.2 Return a receive-only channel
-	return al.errorCh // 2.4.1 - Return the error channel
+func (al Alog) ErrorChannel() <-chan error {
+	return al.errorCh
 }
 
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
+// 3.4 - Implement the Stop() method
 func (al Alog) Stop() {
+	// 3.4.1 - Send a message on the shutdown channel to initiate the shutdown process
+	al.shutdownCh <- struct{}{}
+	// 3.4.2 - Wait for the shutdown complete channel to be closed
+	<-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
